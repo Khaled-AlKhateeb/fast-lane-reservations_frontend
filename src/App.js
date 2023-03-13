ffeb9cdff6ba4dc210cb986f1b03fd158bf15ad4
jaamanu <kennyegun240@gmail.com>
2023-03-14 00:11:40 +0100

    Add routes to login and registration pages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Login from './components/pages/Login';
+import Splash from './components/pages/Splash';
+import Register from './components/pages/Register';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route index element={<Splash />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
+
   );
 }
 
