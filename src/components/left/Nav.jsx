f7ee9e5b939f5132465a84777930e689eed53439
Kenny Egun <kennyegun240@gmail.com>
2023-03-15 00:48:29 +0100

    Modify nav and create dumb reservation page

--- src/components/left/Nav.jsx
+++ src/components/left/Nav.jsx
@@ -8,16 +8,17 @@ import './style/nav.css';
 
 const Nav = () => (
   <div className="NavDiv">
+    <h2 className="NavH2">Fast-Lane</h2>
     <nav>
       <ul>
         <Link to="/" className="link">
           <RiHome7Line />
           Home
         </Link>
-        <li className="link">
+        <Link to="/reservations" className="link">
           <RiReservedFill />
           Reservations
-        </li>
+        </Link>
         <li className="link">
           <RiMotorbikeFill />
           Vehicles
