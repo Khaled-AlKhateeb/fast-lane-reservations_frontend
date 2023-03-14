91557397fbbd242529881d14633fe9ea80b116d4
Kenny Egun <kennyegun240@gmail.com>
2023-03-14 20:34:50 +0100

    Fix name error

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Login from './components/pages/Login';
-import Splash from './components/pages/Splash';
-import Register from './components/pages/Register';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route index element={<Splash />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+
     </div>
 
   );
