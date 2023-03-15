19e60c325baafa1fdaffef42df5ec99b010f4def
Kenny Egun <kennyegun240@gmail.com>
2023-03-15 15:33:31 +0100

    Style vehicles component

--- src/components/pages/Vehicles.jsx
+++ src/components/pages/Vehicles.jsx
@@ -1,9 +1,39 @@
 /* eslint-disable */
 import React from 'react'
+import './styles/vehicle.css'
+import img from '../../assets/bike.jpg'
 
 const Vehicles = () => {
+    const vehicles = [
+        {
+            image: img,
+            name: 'Toyota',
+            desc: 'Just a random vehicle with nice specs'
+        },
+        {
+            image: img,
+            name: 'Toyota',
+            desc: 'Just a random vehicle with nice specs'
+        },
+        {
+            image: img,
+            name: 'Toyota',
+            desc: 'Just a random vehicle with nice specs'
+        }
+    ]
     return (
-        <div className="reservationsDiv">Vehicles</div>
+        <div className="vehiclesDiv">
+            <h2>All Vehicles</h2>
+            <div className="vehicles">
+                {vehicles.map((veh) => (
+                    <div className="vehicleDiv">
+                        <img src={veh.image} className="vehicleImg" alt="" />
+                        <h3>{veh.name}</h3>
+                        <p>{veh.desc}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
 
