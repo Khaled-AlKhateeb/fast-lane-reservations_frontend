2eba0ad7f5e83eaa8173f2f7c466d5cc0669a9ec
Kenny Egun <kennyegun240@gmail.com>
2023-03-15 13:12:49 +0100

    Redirect unlogged users to splash page

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
 /* eslint-disable */
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Reservations from './components/pages/Reservations';
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 import Splash from './components/auth/Splash'
 import Nav from './components/left/Nav';
+import Vehicles from './components/pages/Vehicles';
 
 function App() {
-  const currentUser = false;
+  const currentUser = true;
   return (
     <div className="AppDiv">
-      {currentUser
+      {/* {currentUser
         ?
-        (
-          <div className="App">
-            <div className="navApp">
-              <Nav />
-            </div>
-            <div className="navRoute">
-              <Routes>
-                <Route path="/reservations" element={<Reservations />} />
-              </Routes>
-            </div>
-          </div>
-        )
-        :
-        (
+        ( */}
+      <div className="App">
+        {currentUser && <div className="navApp">
+          <Nav />
+        </div>}
+        <div className="navRoute">
           <Routes>
+            <Route path={"/reservations"} element={currentUser ? <Reservations /> : <Navigate to="/" />} />
+            <Route index element={currentUser ? <Vehicles /> : <Splash />} />
+            <Route path="/login" element={currentUser ? <Navigate to="/" /> : <Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </div>
+      {/* )
+        :
+        ( */}
+      {/* <Routes>
             <Route index element={<Splash />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-          </Routes>
-        )}
+          </Routes> */}
+      {/* )} */}
 
     </div>
 
