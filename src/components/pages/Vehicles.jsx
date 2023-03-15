d72eb43b5a969d5710244cec71dd90976c725d69
Kenny Egun <kennyegun240@gmail.com>
2023-03-15 13:13:51 +0100

    Modify

---
+++ src/components/pages/Vehicles.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable */
+import React from 'react'
+
+const Vehicles = () => {
+    return (
+        <div className="reservationsDiv">Vehicles</div>
+    )
+}
+
+export default Vehicles
