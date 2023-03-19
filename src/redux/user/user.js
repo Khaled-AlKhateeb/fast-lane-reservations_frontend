b02b27fa0c81b8d70bbeae8409c23fc1ad773265
Kenny Egun <kennyegun240@gmail.com>
2023-03-19 12:12:35 +0100

    store data to local storage

--- src/redux/user/user.js
+++ src/redux/user/user.js
@@ -2,25 +2,29 @@
 import { createSlice } from "@reduxjs/toolkit"
 
 const userSlice = createSlice({
-    name: "user",
-    initialState: {
-        currentUser: null,
-        isFetching: false,
-        error: false,
+  name: "user",
+  initialState: {
+    currentUser: null,
+    isFetching: false,
+    error: false,
+  },
+  reducers: {
+    loginStart: (state) => {
+      state.isFetching = true
     },
-    reducers: {
-        loginStart: (state) => {
-            state.isFetching = true
-        },
-        loginSuccess: (state, action) => {
-            state.isFetching = false
-            state.currentUser = action.payload
-        },
-        loginFailure: (state) => {
-            state.isFetching = false
-            state.error = true
-        },
-    }
+    loginSuccess: (state, action) => {
+      state.isFetching = false
+      const token = action.payload
+      localStorage.setItem('token', JSON.stringify(token))
+      const getToken = JSON.parse(localStorage.getItem('token'))
+
+      state.currentUser = getToken
+    },
+    loginFailure: (state) => {
+      state.isFetching = false
+      state.error = true
+    },
+  }
 })
 
 export const { loginStart, loginSuccess, loginFailure } = userSlice.actions;
