66142fc31628654549e70a4fb9642755063f0b92
Degisew <jegisew21@gmail.com>
2023-03-21 01:15:50 +0300

    Added a layout for displaying detail data

--- src/components/pages/VehicleDetails.jsx
+++ src/components/pages/VehicleDetails.jsx
@@ -1,30 +1,43 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import './styles/vehicleDetails.css';
 
 const VehicleDetails = () => {
+  const { state } = useLocation();
 
-const { state } = useLocation();
-
-return (
-  <div>
-    <img src={state.image} alt="" style={{ width: "200px" }} />
-    <p>
-      <b>Name: </b> {state.name}
-    </p>
-    <p>
-      <b>Model: </b> {state.model}
-    </p>
-    <p>
-      <b>Price: </b> {state.price}
-    </p>
-    <p>
-      <b>Horse Power: </b> {state.horse_power}
-    </p>
-    <p>
-      <b>Product Date: </b> {state.year}
-    </p>
-  </div>
-);
+  return (
+    <>
+      <div className="detailsDiv">
+        <img src={state.image} alt="" />
+        <div className="textDetailsDiv">
+          <article className="item">
+            <p className="label">Name: </p>
+            <p className="value">{state.name}</p>
+          </article>
+          <article className="item">
+            <p className="label">Model: </p>
+            <p className="value">{state.model}</p>
+          </article>
+          <article className="item">
+            <p className="label">Price: </p>
+            <p className="value">{state.price}</p>
+          </article>
+          <article className="item">
+            <p className="label">Horse Power: </p>
+            <p className="value">{state.horse_power}</p>
+          </article>
+          <article className="item">
+            <p className="label">Product Date: </p>
+            <p className="value">{state.year}</p>
+          </article>
+          <br />
+          <a href="./reserve">
+            <div className="action">Reserve</div>
+          </a>
+        </div>
+      </div>
+    </>
+  );
 };
 
-export { VehicleDetails };
+export default VehicleDetails;
