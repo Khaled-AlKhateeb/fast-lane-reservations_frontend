67f9f98d42f8b0c36897340db5c36c77a31d90e3
Kenny Egun <kennyegun240@gmail.com>
2023-03-23 16:48:29 +0100

    Implemented login inputs validation

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -6,23 +5,35 @@ import { login } from '../../redux/apiCalls';
 import './styles/login.css';
 
 const Login = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const dispatch = useDispatch()
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [err, setErr] = useState('');
+  const dispatch = useDispatch();
 
   const handleLogin = (e) => {
-    e.preventDefault()
-     
-    login(dispatch, { email, password });
-    
-  }
+    e.preventDefault();
+    if (email === '' && password === '') {
+      setErr('Email and Password should be valid');
+      return false;
+    }
+    if (email === '') {
+      setErr("Email shouldn't be empty");
+      return false;
+    }
+    if (password === '') {
+      setErr('Password should not be empty');
+      return false;
+    }
+    return login(dispatch, { email, password });
+  };
   return (
     <div className="LoginDiv">
       <form className="LoginForm">
         <h2>Login to your account</h2>
         <input onChange={(e) => setEmail(e.target.value)} type="email" name="" id="" placeholder="Enter your email address" />
         <input onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Enter Password" name="" id="" />
-        <button type="submit" onClick={handleLogin} >Login</button>
+        <button type="submit" onClick={handleLogin}>Login</button>
+        {err && <span>{err}</span>}
         <p>
           Don&apos;t have an account?
           <Link to="/register" className="Loginlink">
@@ -31,7 +42,7 @@ const Login = () => {
         </p>
       </form>
     </div>
-  )
+  );
 };
 
 export default Login;
