ea5d4e7459deaa591b20ecef6d279116c348b0f3
Khaled-Al-Khateeb <khaled.w.khateeb85@gmail.com>
2023-03-23 18:40:54 +0300

    Fix the conflicts about delete button

--- src/components/pages/Reservations.jsx
+++ src/components/pages/Reservations.jsx
@@ -1,55 +1,89 @@
 /* eslint-disable */
 import "./../pages/styles/reservations.css";
-
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getReservations } from "../../redux/reservations/reservation";
+import {
+  deleteReserv,
+  getReservations,
+} from "../../redux/reservations/reservation";
 import { useEffect } from "react";
+import { deleteReservations } from "../../redux/apiCalls";
+import { async } from "q";
+
 const Reservations = () => {
   const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
   const reservations = useSelector((state) => state.reservations);
   const [allReservations, setReservations] = useState([]);
   const [loading, setLoading] = useState(true);
-  useEffect(() => {
-  if (currentUser?.data.token) {
-    setLoading(true);
-    dispatch(getReservations({ token: currentUser.data.token })).finally(() => {
-      // Set loading state to false after a 5-second delay
-      setTimeout(() => {
-        setLoading(false);
-      }, 5000);
-    });
-  }
-}, [currentUser, dispatch]);
-
+  const [deleting, setDeleting] = useState(false);
 
   useEffect(() => {
-    if(reservations && reservations.reservations) {
-       setLoading(true);
-        setReservations(reservations.reservations);
+    if (currentUser?.data.token) {
+      setLoading(true);
+      dispatch(getReservations({ token: currentUser.data.token })).finally(
+        () => {
+          // Set loading state to false after a 5-second delay
+          setTimeout(() => {
+            setLoading(false);
+          }, 2000);
+        },
+      );
     }
-}, [reservations]);
+  }, [currentUser, dispatch]);
+
+  useEffect(() => {
+    if (reservations && reservations.reservations) {
+      setLoading(true);
+      setReservations(reservations.reservations);
+    }
+  }, [reservations]);
+
+  const deleteReservation = async (reservation) => {
+    setDeleting(true); // Set loading state to true
+    try {
+      await dispatch(
+        deleteReserv({ token: currentUser.data.token, id: reservation.id }),
+      );
+      dispatch(getReservations({ token: currentUser.data.token })); // Reload reservations after deletion
+    } catch (error) {
+      console.log("Error deleting reservation", error);
+    } finally {
+      const remainingReservations = allReservations.filter(
+        (r) => r.id !== reservation.id,
+      );
+      setReservations(remainingReservations);
+    }
+  };
 
   return (
-    <div className="reservation-main-container">
+    <div className='reservation-main-container'>
       {loading && <h1>Loading Your Reservations..</h1>}
       {!loading && (
         <>
-          <h2 className="reservation-title">Your Reservations</h2>
-          <div className="reservations-div">
+          <h2 className='reservation-title'>Your Reservations</h2>
+          <div className='reservations-div'>
             {allReservations &&
               allReservations.map((reservation) => (
-                <div className="reservations-container" key={reservation.id}>
-                  <div className="reservation-div">
-                    <img src={reservation.vehicle.image} alt={reservation.vehicle.name} />
+                <div
+                  className='reservations-container'
+                  key={reservation.id}
+                >
+                  <div className='reservation-div'>
+                    <img
+                      src={reservation.vehicle.image}
+                      alt={reservation.vehicle.name}
+                    />
                     <span>{reservation.vehicle.name}</span>
                     <span>{reservation.vehicle.model}</span>
                     <p>Reservation ID: {reservation.id}</p>
                     <p>Start Date: {reservation.from_date}</p>
                     <p>End Date: {reservation.to_date}</p>
                     <p>Number of people: {reservation.number_of_person}</p>
-                    <button className="reservation-delete" onClick={() => deleteReservation(reservation)}>
+                    <button
+                      className='reservation-delete'
+                      onClick={() => deleteReservation(reservation)}
+                    >
                       Delete
                     </button>
                   </div>
